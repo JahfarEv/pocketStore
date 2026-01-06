ed62de7c0ba2ddc82237b39f42e82caf408c2426
Jahfar Ali <evjahfar@gmail.com>
2026-01-06 23:17:07 +0530

    footer_updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,48 +1,132 @@
-import React from 'react';
-import { FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import { FaFacebookSquare, FaLinkedin } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const Footer = ({ navigateTo }) => {
+  const navigate = useNavigate();
+
   return (
     <footer className="bg-gray-800 text-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
           <div>
             <h3 className="text-xl font-bold mb-4">Pocket Store</h3>
-            <p className="text-gray-400">The ultimate multi-user shopping platform connecting customers and shops.</p>
+            <p className="text-gray-400">
+              The ultimate multi-user shopping platform connecting customers and
+              shops.
+            </p>
           </div>
-          
+
           <div>
             <h4 className="text-lg font-semibold mb-4">Quick Links</h4>
             <ul className="space-y-2">
-              <li><button onClick={() => navigateTo('home')} className="text-gray-400 hover:text-white transition">Home</button></li>
-              <li><a href="#features" className="text-gray-400 hover:text-white transition">Features</a></li>
-              <li><a href="#how-it-works" className="text-gray-400 hover:text-white transition">How It Works</a></li>
-              <li><a href="#user-types" className="text-gray-400 hover:text-white transition">For Users</a></li>
+              <li>
+                <button
+                  onClick={() => navigateTo("home")}
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  Home
+                </button>
+              </li>
+              <li>
+                <a
+                  href="#features"
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  Features
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#how-it-works"
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  How It Works
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#user-types"
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  For Users
+                </a>
+              </li>
             </ul>
           </div>
-          
+
           <div>
             <h4 className="text-lg font-semibold mb-4">Support</h4>
             <ul className="space-y-2">
-              <li><a href="#" className="text-gray-400 hover:text-white transition">Help Center</a></li>
-              <li><a href="#" className="text-gray-400 hover:text-white transition">FAQs</a></li>
-              <li><button onClick={() => navigateTo('contact')} className="text-gray-400 hover:text-white transition">Contact Us</button></li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  Help Center
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  FAQs
+                </a>
+              </li>
+              <li>
+                <button
+                  onClick={() => navigateTo("contact")}
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  Contact Us
+                </button>
+              </li>
             </ul>
           </div>
-          
+          <div>
+            <h4 className="text-lg font-semibold mb-4">Support</h4>
+            <ul className="space-y-2">
+              <li>
+                <button
+                  onClick={() => navigate("/terms-and-conditions")}
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  Terms And Condition
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => navigate("/privacypolicy")}
+                  className="text-gray-400 hover:text-white transition"
+                >
+                  Privacy Policy
+                </button>
+              </li>{" "}
+            </ul>
+          </div>
+
           <div>
             <h4 className="text-lg font-semibold mb-4">Connect With Us</h4>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 hover:text-white transition"><FaFacebookSquare /></a>
-              <a href="#" className="text-gray-400 hover:text-white transition"><FaSquareXTwitter /></a>
-              <a href="#" className="text-gray-400 hover:text-white transition"><BsInstagram /></a>
-              <a href="#" className="text-gray-400 hover:text-white transition"><FaLinkedin /></a>
+              <a href="#" className="text-gray-400 hover:text-white transition">
+                <FaFacebookSquare />
+              </a>
+              <a href="#" className="text-gray-400 hover:text-white transition">
+                <FaSquareXTwitter />
+              </a>
+              <a href="#" className="text-gray-400 hover:text-white transition">
+                <BsInstagram />
+              </a>
+              <a href="#" className="text-gray-400 hover:text-white transition">
+                <FaLinkedin />
+              </a>
             </div>
           </div>
         </div>
-        
+
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-400">
           <p>&copy; 2025 PoketStor. All rights reserved.</p>
         </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,99 @@
+// // import React from 'react';
+// // import { FaBars, FaTimes } from 'react-icons/fa';
+
+// // const Navigation = ({ currentPage, navigateTo, isMenuOpen, toggleMenu }) => {
+// //   return (
+// //     <nav className="bg-white shadow-lg fixed w-full z-50">
+// //       <div className="max-w-7xl mx-auto px-4">
+// //         <div className="flex justify-between h-16">
+// //           <div className="flex space-x-7">
+// //             <div className="flex items-center">
+// //               <button 
+// //                 onClick={() => navigateTo('home')} 
+// //                 className="text-indigo-600 font-bold text-2xl font-aparajita"
+// //               >
+// //                 PoketStor
+// //               </button>
+// //             </div>
+// //           </div>
+// //           <div className="hidden md:flex items-center space-x-3">
+// //             <button 
+// //               onClick={() => navigateTo('home')} 
+// //               className={`py-2 px-4 ${currentPage === 'home' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+// //             >
+// //               Home
+// //             </button>
+// //             {currentPage === 'home' && (
+// //               <>
+// //                 <a href="#features" className="py-2 px-4 text-gray-700 hover:text-indigo-600">Features</a>
+// //                 <a href="#how-it-works" className="py-2 px-4 text-gray-700 hover:text-indigo-600">How It Works</a>
+// //                 <a href="#user-types" className="py-2 px-4 text-gray-700 hover:text-indigo-600">For Users</a>
+// //               </>
+// //             )}
+// //             <button 
+// //               onClick={() => navigateTo('contact')} 
+// //               className={`py-2 px-4 ${currentPage === 'contact' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+// //             >
+// //               Contact Us
+// //             </button>
+// //             <a href="#download" className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition">Download Now</a>
+// //           </div>
+// //           <div className="md:hidden flex items-center">
+// //             <button onClick={toggleMenu} className="outline-none">
+// //               {isMenuOpen ? <FaTimes className="w-6 h-6 text-gray-700" /> : <FaBars className="w-6 h-6 text-gray-700" />}
+// //             </button>
+// //           </div>
+// //         </div>
+// //       </div>
+      
+// //       {/* Mobile Menu */}
+// //       {isMenuOpen && (
+// //         <div className="md:hidden bg-white px-4 pt-2 pb-4">
+// //           <button 
+// //             onClick={() => navigateTo('home')} 
+// //             className={`block py-2 w-full text-left ${currentPage === 'home' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+// //           >
+// //             Home
+// //           </button>
+// //           {currentPage === 'home' && (
+// //             <>
+// //               <a href="#features" className="block py-2 text-gray-700 hover:text-indigo-600">Features</a>
+// //               <a href="#how-it-works" className="block py-2 text-gray-700 hover:text-indigo-600">How It Works</a>
+// //               <a href="#user-types" className="block py-2 text-gray-700 hover:text-indigo-600">For Users</a>
+// //             </>
+// //           )}
+// //           <button 
+// //             onClick={() => navigateTo('contact')} 
+// //             className={`block py-2 w-full text-left ${currentPage === 'contact' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+// //           >
+// //             Contact Us
+// //           </button>
+// //           <a href="#download" className="block bg-indigo-600 text-white px-4 py-2 rounded-md mt-2 hover:bg-indigo-700 transition">Download Now</a>
+// //         </div>
+// //       )}
+// //     </nav>
+// //   );
+// // };
+
+// // export default Navigation;  
+
+
 // import React from 'react';
 // import { FaBars, FaTimes } from 'react-icons/fa';
 
 // const Navigation = ({ currentPage, navigateTo, isMenuOpen, toggleMenu }) => {
 //   return (
-//     <nav className="bg-white shadow-lg fixed w-full z-50">
+//     <nav className="bg-indigo-700 shadow-lg fixed w-full z-50">
 //       <div className="max-w-7xl mx-auto px-4">
 //         <div className="flex justify-between h-16">
 //           <div className="flex space-x-7">
 //             <div className="flex items-center">
-//               <button 
+//               <div 
 //                 onClick={() => navigateTo('home')} 
-//                 className="text-indigo-600 font-bold text-2xl font-aparajita"
+//                 className="text-white font-bold text-4xl font-aparajita"
 //               >
-//                 PoketStor
-//               </button>
+//                 Poket<span className='text-yellow-400'>Stor</span>
+//               </div>
 //             </div>
 //           </div>
 //           <div className="hidden md:flex items-center space-x-3">
@@ -25,18 +105,24 @@
 //             </button>
 //             {currentPage === 'home' && (
 //               <>
-//                 <a href="#features" className="py-2 px-4 text-gray-700 hover:text-indigo-600">Features</a>
-//                 <a href="#how-it-works" className="py-2 px-4 text-gray-700 hover:text-indigo-600">How It Works</a>
-//                 <a href="#user-types" className="py-2 px-4 text-gray-700 hover:text-indigo-600">For Users</a>
+//                 <a href="#features" className="py-2 px-4 text-white hover:text-gray-300">Features</a>
+//                 <a href="#how-it-works" className="py-2 px-4 text-white hover:text-gray-300">How It Works</a>
+//                 <a href="#user-types" className="py-2 px-4 text-white hover:text-gray-300">For Users</a>
 //               </>
 //             )}
 //             <button 
+//               onClick={() => navigateTo('careers')} 
+//               className={`py-2 px-4 ${currentPage === 'careers' ? 'text-white' : 'text-gray-700 hover:text-gray-300'}`}
+//             >
+//               Careers
+//             </button>
+//             <button 
 //               onClick={() => navigateTo('contact')} 
-//               className={`py-2 px-4 ${currentPage === 'contact' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+//               className={`py-2 px-4 ${currentPage === 'contact' ? 'text-white' : 'text-gray-700 hover:text-gray-300'}`}
 //             >
 //               Contact Us
 //             </button>
-//             <a href="#download" className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition">Download Now</a>
+//             {/* <a href="#download" className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition">Download Now</a> */}
 //           </div>
 //           <div className="md:hidden flex items-center">
 //             <button onClick={toggleMenu} className="outline-none">
@@ -51,7 +137,7 @@
 //         <div className="md:hidden bg-white px-4 pt-2 pb-4">
 //           <button 
 //             onClick={() => navigateTo('home')} 
-//             className={`block py-2 w-full text-left ${currentPage === 'home' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+//             className={`block py-2 w-full text-left ${currentPage === 'home' ? 'text-white' : 'text-gray-700 hover:text-indigo-600'}`}
 //           >
 //             Home
 //           </button>
@@ -63,8 +149,14 @@
 //             </>
 //           )}
 //           <button 
+//             onClick={() => navigateTo('careers')} 
+//             className={`block py-2 w-full text-left ${currentPage === 'careers' ? 'text-white' : 'text-gray-700 hover:text-indigo-600'}`}
+//           >
+//             Careers
+//           </button>
+//           <button 
 //             onClick={() => navigateTo('contact')} 
-//             className={`block py-2 w-full text-left ${currentPage === 'contact' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+//             className={`block py-2 w-full text-left ${currentPage === 'contact' ? 'text-white' : 'text-gray-700 hover:text-indigo-600'}`}
 //           >
 //             Contact Us
 //           </button>
@@ -75,58 +167,75 @@
 //   );
 // };
 
-// export default Navigation;  
-
+// export default Navigation;
 
 import React from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navigation = ({ currentPage, navigateTo, isMenuOpen, toggleMenu }) => {
   return (
-    <nav className="bg-white shadow-lg fixed w-full z-50">
+    <nav className="bg-indigo-700 shadow-lg fixed w-full z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-16">
           <div className="flex space-x-7">
             <div className="flex items-center">
-              <button 
+              <div 
                 onClick={() => navigateTo('home')} 
-                className="text-indigo-600 font-bold text-2xl font-aparajita"
+                className="text-white font-bold text-4xl font-aparajita cursor-pointer"
               >
-                PoketStor
-              </button>
+                Poket<span className='text-yellow-400'>Stor</span>
+              </div>
             </div>
           </div>
+          
+          {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-3">
             <button 
               onClick={() => navigateTo('home')} 
-              className={`py-2 px-4 ${currentPage === 'home' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+              className={`py-2 px-4 font-medium ${currentPage === 'home' ? 'text-yellow-300' : 'text-white hover:text-yellow-300'}`}
             >
               Home
             </button>
+            
             {currentPage === 'home' && (
               <>
-                <a href="#features" className="py-2 px-4 text-gray-700 hover:text-indigo-600">Features</a>
-                <a href="#how-it-works" className="py-2 px-4 text-gray-700 hover:text-indigo-600">How It Works</a>
-                <a href="#user-types" className="py-2 px-4 text-gray-700 hover:text-indigo-600">For Users</a>
+                <a href="#features" className="py-2 px-4 font-medium text-white hover:text-yellow-300">Features</a>
+                <a href="#how-it-works" className="py-2 px-4 font-medium text-white hover:text-yellow-300">How It Works</a>
+                <a href="#user-types" className="py-2 px-4 font-medium text-white hover:text-yellow-300">For Users</a>
               </>
             )}
+            
             <button 
               onClick={() => navigateTo('careers')} 
-              className={`py-2 px-4 ${currentPage === 'careers' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+              className={`py-2 px-4 font-medium ${currentPage === 'careers' ? 'text-yellow-300' : 'text-white hover:text-yellow-300'}`}
             >
               Careers
             </button>
+            
             <button 
               onClick={() => navigateTo('contact')} 
-              className={`py-2 px-4 ${currentPage === 'contact' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+              className={`py-2 px-4 font-medium ${currentPage === 'contact' ? 'text-yellow-300' : 'text-white hover:text-yellow-300'}`}
             >
               Contact Us
             </button>
-            {/* <a href="#download" className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition">Download Now</a> */}
+            
+            {/* Download Now button if needed */}
+            {/* <button className="bg-yellow-400 text-indigo-700 font-bold px-6 py-2 rounded-lg hover:bg-yellow-300 transition duration-300">
+              Download Now
+            </button> */}
           </div>
+          
+          {/* Mobile Menu Button */}
           <div className="md:hidden flex items-center">
-            <button onClick={toggleMenu} className="outline-none">
-              {isMenuOpen ? <FaTimes className="w-6 h-6 text-gray-700" /> : <FaBars className="w-6 h-6 text-gray-700" />}
+            <button 
+              onClick={toggleMenu} 
+              className="outline-none p-2"
+            >
+              {isMenuOpen ? (
+                <FaTimes className="w-6 h-6 text-white" />
+              ) : (
+                <FaBars className="w-6 h-6 text-white" />
+              )}
             </button>
           </div>
         </div>
@@ -134,33 +243,67 @@ const Navigation = ({ currentPage, navigateTo, isMenuOpen, toggleMenu }) => {
       
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="md:hidden bg-white px-4 pt-2 pb-4">
+        <div className="md:hidden bg-indigo-800 px-4 pt-2 pb-4">
           <button 
-            onClick={() => navigateTo('home')} 
-            className={`block py-2 w-full text-left ${currentPage === 'home' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+            onClick={() => {
+              navigateTo('home');
+              toggleMenu();
+            }} 
+            className={`block py-3 w-full text-left font-medium ${currentPage === 'home' ? 'text-yellow-300' : 'text-white hover:text-yellow-300'}`}
           >
             Home
           </button>
+          
           {currentPage === 'home' && (
             <>
-              <a href="#features" className="block py-2 text-gray-700 hover:text-indigo-600">Features</a>
-              <a href="#how-it-works" className="block py-2 text-gray-700 hover:text-indigo-600">How It Works</a>
-              <a href="#user-types" className="block py-2 text-gray-700 hover:text-indigo-600">For Users</a>
+              <a 
+                href="#features" 
+                onClick={toggleMenu}
+                className="block py-3 font-medium text-white hover:text-yellow-300"
+              >
+                Features
+              </a>
+              <a 
+                href="#how-it-works" 
+                onClick={toggleMenu}
+                className="block py-3 font-medium text-white hover:text-yellow-300"
+              >
+                How It Works
+              </a>
+              <a 
+                href="#user-types" 
+                onClick={toggleMenu}
+                className="block py-3 font-medium text-white hover:text-yellow-300"
+              >
+                For Users
+              </a>
             </>
           )}
+          
           <button 
-            onClick={() => navigateTo('careers')} 
-            className={`block py-2 w-full text-left ${currentPage === 'careers' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+            onClick={() => {
+              navigateTo('careers');
+              toggleMenu();
+            }} 
+            className={`block py-3 w-full text-left font-medium ${currentPage === 'careers' ? 'text-yellow-300' : 'text-white hover:text-yellow-300'}`}
           >
             Careers
           </button>
+          
           <button 
-            onClick={() => navigateTo('contact')} 
-            className={`block py-2 w-full text-left ${currentPage === 'contact' ? 'text-indigo-600' : 'text-gray-700 hover:text-indigo-600'}`}
+            onClick={() => {
+              navigateTo('contact');
+              toggleMenu();
+            }} 
+            className={`block py-3 w-full text-left font-medium ${currentPage === 'contact' ? 'text-yellow-300' : 'text-white hover:text-yellow-300'}`}
           >
             Contact Us
           </button>
-          <a href="#download" className="block bg-indigo-600 text-white px-4 py-2 rounded-md mt-2 hover:bg-indigo-700 transition">Download Now</a>
+          
+          {/* Optional Download button for mobile */}
+          {/* <button className="block w-full bg-yellow-400 text-indigo-700 font-bold px-4 py-3 rounded-lg mt-2 hover:bg-yellow-300 transition duration-300">
+            Download Now
+          </button> */}
         </div>
       )}
     </nav>
